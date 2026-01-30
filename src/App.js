d155aac55bed14d127846f24155bda88d68f7114
mitchn5 <michaeln@bell.net>
2026-01-30 17:01:12 -0500

    real first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+// import profile from './placeholder.jpg';
+// import appStoreLogo from './apple_store.png';
+
+// import Flipping from './Flipping.js';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+const AMAYA = 0;
+const MITCH = 1;
+var lightMode = false;
+
+function toggleMode() {
+    var element = document.body;
+    lightMode = !lightMode;
+    element.classList.toggle("light-mode");
+}
+
+function Home() {
+    return (
+        <div class="main-container">
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+// TODO AMAYA: make login real pretty like you :) 
+function Login() {
+    return (
+        <div class="main-container">
+          <div class="content-container">
+            <h1>On Air</h1>
+            <div>
+              <input></input>
+              <input></input>
+              <button class="login-button">login</button>
+            </div>
+          </div>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            {/* <div class="topnav">
+                {window.innerWidth > 500 ?
+                    <div>
+                        <Link to="/login">Login</Link>
+                    </div>
+                    : null
+                }
+                <label class="switch">
+                    <input type="checkbox" onClick={toggleMode} />
+                    <span class="slider round"></span>
+                </label>
+            </div> */}
+            <div>
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/" element={<Login />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
