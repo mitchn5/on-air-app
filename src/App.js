004f764114f5c77b101e4d3c63f696da186f7203
mitchn5 <michaeln@bell.net>
2026-01-30 18:10:29 -0500

    updated login page to redirect to home

--- src/App.js
+++ src/App.js
@@ -3,46 +3,57 @@
 
 // import Flipping from './Flipping.js';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from "react-router-dom";
 
 const AMAYA = 0;
 const MITCH = 1;
 var lightMode = false;
 
 function toggleMode() {
-    var element = document.body;
-    lightMode = !lightMode;
-    element.classList.toggle("light-mode");
+  var element = document.body;
+  lightMode = !lightMode;
+  element.classList.toggle("light-mode");
 }
 
 function Home() {
-    return (
-        <div class="main-container">
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div class="main-container">
+      <h1>Home</h1>
+    </div>
+  );
 }
 
 // TODO AMAYA: make login real pretty like you :) 
 function Login() {
-    return (
-        <div class="main-container">
-          <div class="content-container">
-            <h1>On Air</h1>
-            <div>
-              <input></input>
-              <input></input>
-              <button class="login-button">login</button>
-            </div>
-          </div>
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    const success = true; //await loginUser(); // your auth logic
+
+    if (success) {
+      navigate('/home', { replace: true });
+    }
+  };
+
+  return (
+    <div class="main-container">
+      <div class="content-container">
+        <h1>On Air</h1>
+        <div class="login-container">
+          <input placeholder='username'></input>
+          <input placeholder='password'></input>
+          <button onClick={handleLogin}>login</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 function App() {
-    return (
-        <Router>
-            {/* <div class="topnav">
+  return (
+    <Router>
+      {/* <div class="topnav">
                 {window.innerWidth > 500 ?
                     <div>
                         <Link to="/login">Login</Link>
@@ -54,14 +65,14 @@ function App() {
                     <span class="slider round"></span>
                 </label>
             </div> */}
-            <div>
-                <Routes>
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/" element={<Login />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+      <div>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
